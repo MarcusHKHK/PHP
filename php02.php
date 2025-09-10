a9a0c06b74a4bb0124ac8f5f85c863f53bd30b3d
MarcusHKHK <marcus@hkhk.edu.ee>
2025-09-10 11:23:29 +0300

    Update php02.php

--- php02.php
+++ php02.php
@@ -7,8 +7,127 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    <h1>Harjutus 7.6</h1>
+    <!-- Harjutus 07
+    10.09.25
+    Marcus Krutto -->
+    <!-- uudis -->
 
-    <h1>Harjutus 06.2</h1>
+    <?php
+    function isikukood(){
+      echo'    <form action="#" method="get">
+        Sisesta idkood <input type="number" name="idkood" required><br>
+        <input type="submit" value="Kontrolli">
+    </form>';
+      if(isset($_GET["idkood"])){
+        $ikood = $_GET["idkood"];
+        if (strlen($ikood) == 11){
+          echo "Isikukood on korrektne, ";
+        } else{
+          echo "Kontrollige kas isikukood on õige";
+        }
+        if ($ik = substr($ikood,0,1)%2 == 0){
+          echo " Naine ,";
+        } else{
+          echo " Mees ,";
+        }
+        $paev = $ik = substr($ikood,5,6);
+        $kuu = $ik = substr($ikood,3,5);
+        $aasta = $ik = substr($ikood,1,2);
+        echo "Sünni aeg: $paev.$kuu.$aasta";
+      }
+    }
+    isikukood();
+    ?>
+    <h1>Harjutus 7.5</h1>
+    <!-- Harjutus 07
+    10.09.25
+    Marcus Krutto -->
+    <!-- uudis -->
+
+    <?php
+    function ristkylik($kylga, $kylgb){
+      $pindala =$kylga*$kylgb;
+      echo $pindala;
+    }
+    ristkylik(5,10);
+    ?>
+    <h1>Harjutus 7.4</h1>
+    <!-- Harjutus 07
+    10.09.25
+    Marcus Krutto -->
+    <!-- uudis -->
+
+    <?php
+    function vhmik($a1, $a2, $s){
+      for ($i=$a1; $i <= $a2; $i=$i+$s){
+        echo $i;
+      }
+    }
+    vhmik(2,20,3)
+    ?>
+    <h1>Harjutus 7.3</h1>
+    <!-- Harjutus 07
+    10.09.25
+    Marcus Krutto -->
+    <!-- uudis -->
+
+    <?php
+    function createUser($u){
+      $lu = strtolower($u);
+      echo $lu."@hkhk.edu.ee"."<br>";
+      $p = substr(password_hash($u, PASSWORD_BCRYPT),7,7);
+      echo "Password: ".$p;
+    }
+    createUser("Marcus");
+    ?>
+    <h1>Harjutus 7.2</h1>
+    <!-- Harjutus 07
+    10.09.25
+    Marcus Krutto -->
+    <!-- uudis -->
+
+    <?php
+    function uudiskiri(){
+      echo '    <div class="row">
+      <div class="col-sm-2">
+        <form action="#" method="get">
+          <input type="text" placeholder="Liitu uudiskirjaga">
+          <input type="submit" value="Liitu" class="btn btn-success">
+        </form>
+      </div>
+    </div>';
+    }
+    uudiskiri();
+    ?>
+
+    <h1>Harjutus 7</h1>
+    <!-- Harjutus 07
+    10.09.25
+    Marcus Krutto -->
+    <!-- tervitus -->
+    <?php
+    function tervita($nimi="kylaline"){
+      $tervitused = array("Tere hommikust", "Tervitus", "Hommik", "Õhtust", "Tsau", "Tere sõbrake");
+      return array_rand(array_flip($tervitused))." $nimi!"."<br>";
+    }
+    echo tervita();
+    ?>
+
+    <h1>Harjutus 06.7</h1>
+    <!-- Harjutus 06
+    10.09.25
+    Marcus Krutto -->
+    <!-- poisid ja tüdrukud -->
+    <?php
+    $p = array("Juss", "Toomas", "Erki", "Reigo");
+    $t = array("Maria", "Liisa", "Annika", "Janeli");
+
+    for ($i=0; $i < count($t); $i++){
+      echo $t[$i]." - ".$p[$i]."<br>"; 
+    }
+    ?>
+    <h1>Harjutus 06</h1>
     <!-- Harjutus 06
     08.09.25
     Marcus Krutto -->
